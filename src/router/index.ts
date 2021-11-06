b7bdc8a046ec240a65c5b18bd3e6f1c9bc4059a8
chenhai <chenhai@lonsid.cn>
2021-11-06 14:46:05 +0800

    修改.d.ts路径

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,21 @@
 import { App } from 'vue';
-import { PageEnum } from 'enums/pageEnum';
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
-export const RootRoute: RouteRecordRaw = {
-    path: '/',
-    name: 'Root',
-    redirect: PageEnum.BASE_HOME,
-    meta: {
-      title: 'Root',
-    },
-  };
-  
-  export const LoginRoute: RouteRecordRaw = {
-    path: '/login',
-    name: 'Login',
-    component: () => import('views/login/index.vue'),
-    meta: {
-      title: '登录',
-    },
-  };
+export const LoginRoute: RouteRecordRaw = {
+  path: '/login',
+  name: 'Login',
+  component: () => import('views/login/index.vue'),
+  meta: {
+    title: '登录',
+  },
+};
 
 //普通路由 无需验证权限
-export const constantRouter: any[] = [LoginRoute, RootRoute];
+export const constantRouter: any[] = [LoginRoute];
 
 const router = createRouter({
-    history: createWebHashHistory(''),
-    routes: constantRouter,
-    strict: true,
-    scrollBehavior: () => ({ left: 0, top: 0 }),
-  });
+  history: createWebHashHistory(''),
+  routes: constantRouter,
+  strict: true,
+  scrollBehavior: () => ({ left: 0, top: 0 }),
+});
---
+++ src/views/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+
+onMounted(() => {
+
+});
+
+</script>
+
+<style scoped lang="scss">
+</style>
