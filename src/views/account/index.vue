615eedbd1917107070133b60b7e433483eb0163a
msyml <18858713997@163.com>
2021-11-07 14:22:51 +0800

    确定大致代码结构

---
+++ src/views/account/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+
+onMounted(() => {
+
+});
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
